4b31e2035f784a6120848358847394d1a95c918d
GUO CHENXI <guochenxi@GUOs-MacBook-Pro.local>
2020-06-27 05:11:06 +1000

    submit final

--- controllers/product.js
+++ controllers/product.js
@@ -5,7 +5,9 @@ const { errorHandler } = require("../helpers/dbErrorHandler");
 const Product = require("../models/product");
 
 exports.productById = (req, res, next, id) => {
-    Product.findById(id).exec((err, product) => {
+    Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
         if (err || !product) {
             return res.status(400).json({
                 error: "Product not found",
@@ -97,19 +99,7 @@ exports.update = (req, res) => {
             });
         }
 
-        //check for all fields
-        const { name, description, price, category, quantity, shipping } = fields;
-        if (!name ||
-            !description ||
-            !price ||
-            !category ||
-            !quantity ||
-            !shipping
-        ) {
-            return res.status(400).json({
-                error: "All fields are required",
-            });
-        }
+      
 
         let product = req.product;
         product = _.extend(product, fields);
@@ -265,3 +255,50 @@ exports.photo = (req, res, next) => {
     }
     next();
 };
+
+exports.listSearch = (req, res) => {
+    // create query object to hold search value and category value
+    const query = {}
+    // assign search value to query name
+    if(req.query.search){
+        query.name = {$regex: req.query.search, $options: 'i'}
+        // assign category value to query.category
+        if(req.query.category && req.query.category != 'All'){
+            query.category = req.query.category
+        }
+
+        //find the product based on query object with 2 properties
+        // search and category
+        Product.find(query, (err, products) => {
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(products)
+        }).select('-photo')
+    }
+}
+
+exports.decreaseQuantity = (req, res, next) => {
+    let bulkOps = req.body.order.products.map((item) => {
+        return {
+            updateOne: {
+                filter: {_id: item._id},
+                update: {$inc: {quantity: -item.count, sold: +item.count}}
+            }
+        }
+    });
+
+    Product.bulkWrite(bulkOps, {}, (error, products) => {
+        if(error){
+            return res.status(400).json({
+                error: "Could not update product"
+            })
+        }
+        next();
+    })
+
+
+}
+
--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
 
 
-const { userById, read, update } = require("../controllers/user");
+const { userById, read, update, purchaseHistory } = require("../controllers/user");
 
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
@@ -18,6 +18,7 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
 
 
 router.param('userId', userById);
